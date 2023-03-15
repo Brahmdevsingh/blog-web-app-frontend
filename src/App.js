8b55a92527aedc3dcc03879fbe344e06bbbb2eee
Brahmdevsingh <65753877+Brahmdevsingh@users.noreply.github.com>
2023-03-15 23:35:56 +0530

    2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hey there Capgemini Project
+          Capgemini Project
         </a>
       </header>
     </div>
